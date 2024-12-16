11a8e6ab3748431a1acf55c6885bb5f3282c97e8
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2024-12-16 10:41:09 +0600

    Work continue

--- src/Pages/Today.jsx
+++ src/Pages/Today.jsx
@@ -1,42 +1,62 @@
-import React from "react";
-
+import { FaArrowRightLong } from "react-icons/fa6";
+import { FaArrowLeftLong } from "react-icons/fa6";
 const Today = () => {
   return (
     <>
       <div className="container mx-auto">
-        <div className="flex items-center gap-20">
+        <div className="">
           <div className="flex items-center gap-2 mt-14">
             <div className="w-[15px] h-[30px] bg-[#BD4444] rounded-r-md "></div>
             <h2 className="text-[#BD4444] text-xl font-mono font-bold">
               Today
             </h2>
           </div>
-          <div className="mt-10 flex gap-10 ">
-            <div>
-              <h2>Flash Sales</h2>
+          <div className=" flex mt-10   items-center justify-between ">
+            <div className="w-[200px]">
+              <h2 className="text-2xl font-bold">Flash Sales</h2>
             </div>
-            <div className="flex gap-5 text-center ">
-              <div>
-                <h3>Days</h3>
-                <p>03</p>
+            <div className="flex grid grid-cols-5 items-center ">
+              <div className=" ">
+                <h3 className="text-lg font-bold">Days</h3>
+                <div className="flex gap-3 ">
+                  {" "}
+                  <p className="text-xl font-bold ml-2 ">03 </p>
+                  <p className="text-red-500 font-bold text-lg ml-3 ">: </p>
+                </div>
               </div>
-              <div>
-                <h3>Hours</h3>
-                <p>23</p>
+              <div className="">
+                <h3 className="text-lg font-bold">Hours</h3>
+                <div className="flex gap-3">
+                  {" "}
+                  <p className="text-xl font-bold ml-3 ">11 </p>
+                  <p className="text-red-500 font-bold text-lg ml-3 ">: </p>
+                </div>
               </div>
-              <div>
-                <h3>Minutes</h3>
-                <p>19</p>
+              <div className="items-center">
+                <h3 className="text-lg font-bold">Minutes</h3>
+                <div className="flex gap-3">
+                  {" "}
+                  <p className="text-xl font-bold ml-2 ">56 </p>
+                  <p className="text-red-500 font-bold text-lg ml-8">: </p>
+                </div>
               </div>
-              <div className="gap-36">
-                <h3>Seconds</h3>
-                <p>56</p>
+              <div className="">
+                <h3 className="text-lg font-bold ml-5">Second</h3>
+                <div className="flex gap-3">
+                  {" "}
+                  <p className="text-xl font-bold ml-8 ">32 </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="mt-10 flex  ">
-            <div>
-              <h2>Flash Sales</h2>
+            <div className=" flex w-[200px] ml-56  ">
+              <div>
+                <button className="py-2 px-2 bg-red-200 rounded-lg mr-4">
+                  <FaArrowRightLong />
+                </button>
+                <button className="py-2 px-2 bg-red-200 rounded-lg ">
+                  <FaArrowLeftLong />
+                </button>
+              </div>
             </div>
           </div>
         </div>
