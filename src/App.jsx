1bea142c3d789ba5715b9261099b232037a6d1e1
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2024-12-17 16:01:35 +0600

    First Face Almost Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import AddEnhance from "./Pages/AddEnhance";
 import BestSellingProducts from "./Pages/BestSellingProducts";
 import Category from "./Pages/Category";
+import Footer from "./Pages/Footer";
 import Heropart from "./Pages/Heropart";
 import Nabbar from "./Pages/Nabbar";
 import NewArrival from "./Pages/NewArrival";
 import OurProducts from "./Pages/OurProducts";
+import ServiceDelevary from "./Pages/ServiceDelevary";
 import Today from "./Pages/Today";
 
 
@@ -21,6 +23,8 @@ const App = () => {
       <AddEnhance/>
       <OurProducts/>
       <NewArrival/>
+      <ServiceDelevary/>
+      <Footer/>
       </div>
     </>
   );
