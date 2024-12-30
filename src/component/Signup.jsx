de40a4d13f4e935e9285ab38785cb3b5417d0458
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2024-12-30 12:25:35 +0600

    SignUp Auth

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { FaEyeSlash } from "react-icons/fa6";
+import { FaEye } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Signup = () => {
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [nameerror, setNameerror] = useState("");
+  let [emailerror, setEmailerror] = useState("");
+  let [passworderror, setPassworderror] = useState("");
+  let [passwordshow, setPasswordshow] = useState(false);
+
+  let handleName = (e) => {
+    setName(e.target.value);
+    setNameerror("");
+  };
+  let handleEmail = (e) => {
+    setEmail(e.target.value);
+    setEmailerror("");
+  };
+  let handlePassword = (e) => {
+    setPassword(e.target.value);
+    setPassworderror("");
+  };
+  let handleSubmit = () => {
+    if (!name) {
+      setNameerror("Name is required");
+    }
+    if (!email) {
+      setEmailerror("Email is required");
+    }
+    if (!password) {
+      setPassworderror("Password is required");
+    }
+  };
   return (
     <>
       <div className="container mx-auto">
@@ -13,23 +48,51 @@ const Signup = () => {
             </h2>
             <p className="text-[16px] mt-[10px]">Enter your details below</p>
             <input
-              className="px-20 mt-10 py-3  outline-none border-b-2 border-gary-500 block mb-6"
-              placeholder="User Name"
+              onChange={handleName}
+              className="px-20 mt-10 py-3  outline-none border-b-2 border-gary-500 block "
+              placeholder="Name"
               type="text"
+              value={name}
             ></input>
+            {nameerror && (
+              <p className="text-red-500 text-[10px] mb-6">{nameerror}</p>
+            )}
             <input
-              className="px-20 py-3  outline-none border-b-2 border-gary-500 block mb-6"
+              onChange={handleEmail}
+              className="px-20 py-3  outline-none border-b-2 border-gary-500 block "
               placeholder="Email"
               type="text"
+              value={email}
             ></input>
+            {emailerror && (
+              <p className="text-red-500 text-[10px] mb-6">{emailerror}</p>
+            )}
             <input
-              className="px-20 py-3  outline-none border-b-2 border-gary-500 block mb-6"
+              onChange={handlePassword}
+              className="px-20 py-3  outline-none border-b-2 border-gary-500 block relative "
               placeholder="Password"
-              type="text"
+              type={passwordshow ? "text" : "password"}
+              value={password}
             ></input>
-
+            {passwordshow ? (
+              <FaEye
+                onClick={() => setPasswordshow(false)}
+                className="absolate translate-y-[-30px] translate-x-[320px] cursor-pointer"
+              />
+            ) : (
+              <FaEyeSlash
+                onClick={() => setPasswordshow(true)}
+                className="absolate translate-y-[-30px] translate-x-[320px] cursor-pointer"
+              />
+            )}
+            {passworderror && (
+              <p className="text-red-500 text-[10px] mb-6">{passworderror}</p>
+            )}
             <div className=" relative content-start">
-              <button className="bg-[#DB4444] px-[70px] text-white font-semibold py-3 rounded-sm shadow-lg shadow-gray-200 block mb-5">
+              <button
+                onClick={handleSubmit}
+                className="bg-[#DB4444] px-[70px] text-white font-semibold py-3 rounded-sm shadow-lg shadow-gray-200 block mb-5"
+              >
                 {" "}
                 Create Account
               </button>
@@ -41,10 +104,7 @@ const Signup = () => {
               </button>
             </div>
             <div className="flex gap-5 ">
-              <p>Already have account?</p>{" "}
-              <a className="underline hover:font-semibold" href="/login">
-                Log in
-              </a>
+              <p>Already have account?</p> <Link to="/"> Log in</Link>
             </div>
           </div>
         </div>
